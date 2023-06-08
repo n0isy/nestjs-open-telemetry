d2673d6a86247484250f92adef8891571646bf61
sakura <yuuki.shamiko@icloud.com>
2023-06-08 19:30:59 +0800

    refactor: refactor project

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/src'],
-  testMatch: [`**/Tests/*.+(ts|js)`],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-};
--- src/Trace/Decorators/Span.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Constants } from '../../Constants';
-
-export const Span = (name?: string) =>
-  SetMetadata(Constants.TRACE_METADATA, name);
